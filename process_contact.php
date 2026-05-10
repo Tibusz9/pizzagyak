b50d6a23a81c54a4565d97ea8fd4fc036a19d653
bfacskoedit <efackov@gmail.com>
2026-05-10 17:40:51 +0200

    Create process_contact.php

---
+++ process_contact.php
@@ -0,0 +1,7 @@
+<?php
+ob_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
